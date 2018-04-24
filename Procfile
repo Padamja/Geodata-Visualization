c807830d8fe2b01b7efd8301de06a473a5d44299
Padamja <padamja.gupta@gmail.com>
2018-04-24 13:03:57 +0530

    Deploying on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn geovisualization.wsgi
--- geovisualization/settings.py
+++ geovisualization/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import jinja2
 import django_extensions
+import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,9 +29,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')!%5$m+y!hq==efhe*-*t2hy&nes^nj=n2#$c%-(x$8to-55j4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -128,3 +131,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.setting(locals())
